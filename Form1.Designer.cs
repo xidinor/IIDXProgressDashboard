2d0144e04e58e26969705c5cd866c297c5a4faa6
xidinor <serviceinfo@versatile.space>
2026-02-09 23:04:05 +0900

    change main form
    難易度表のランクごとにクリア種別と曲数を表示

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,82 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txtSongName = new TextBox();
-            cmbDifficulty = new ComboBox();
-            btnSearch = new Button();
-            dgvHistory = new DataGridView();
-            btnShowGraph = new Button();
-            ((System.ComponentModel.ISupportInitialize)dgvHistory).BeginInit();
+            cmbLevel = new ComboBox();
+            dgvStats = new DataGridView();
+            btnLoad = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvStats).BeginInit();
             SuspendLayout();
             // 
-            // txtSongName
+            // cmbLevel
             // 
-            txtSongName.Location = new Point(12, 12);
-            txtSongName.Name = "txtSongName";
-            txtSongName.Size = new Size(776, 23);
-            txtSongName.TabIndex = 0;
+            cmbLevel.FormattingEnabled = true;
+            cmbLevel.Items.AddRange(new object[] { "11", "12" });
+            cmbLevel.Location = new Point(12, 12);
+            cmbLevel.Name = "cmbLevel";
+            cmbLevel.Size = new Size(151, 23);
+            cmbLevel.TabIndex = 0;
             // 
-            // cmbDifficulty
+            // dgvStats
             // 
-            cmbDifficulty.FormattingEnabled = true;
-            cmbDifficulty.Items.AddRange(new object[] { "SPN", "SPH", "SPA", "SPL", "DPN", "DPH", "DPA", "DPL" });
-            cmbDifficulty.Location = new Point(12, 54);
-            cmbDifficulty.Name = "cmbDifficulty";
-            cmbDifficulty.Size = new Size(614, 23);
-            cmbDifficulty.TabIndex = 1;
+            dgvStats.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvStats.Location = new Point(12, 41);
+            dgvStats.Name = "dgvStats";
+            dgvStats.Size = new Size(923, 517);
+            dgvStats.TabIndex = 1;
             // 
-            // btnSearch
+            // btnLoad
             // 
-            btnSearch.Location = new Point(632, 54);
-            btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(75, 23);
-            btnSearch.TabIndex = 2;
-            btnSearch.Text = "検索";
-            btnSearch.UseVisualStyleBackColor = true;
-            btnSearch.Click += btnSearch_Click;
-            // 
-            // dgvHistory
-            // 
-            dgvHistory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvHistory.Location = new Point(12, 83);
-            dgvHistory.Name = "dgvHistory";
-            dgvHistory.Size = new Size(776, 355);
-            dgvHistory.TabIndex = 3;
-            // 
-            // btnShowGraph
-            // 
-            btnShowGraph.Location = new Point(713, 54);
-            btnShowGraph.Name = "btnShowGraph";
-            btnShowGraph.Size = new Size(75, 23);
-            btnShowGraph.TabIndex = 4;
-            btnShowGraph.Text = "グラフ表示";
-            btnShowGraph.UseVisualStyleBackColor = true;
-            btnShowGraph.Click += btnShowGraph_Click;
+            btnLoad.Location = new Point(169, 12);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 2;
+            btnLoad.Text = "読み込み";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnShowGraph);
-            Controls.Add(dgvHistory);
-            Controls.Add(btnSearch);
-            Controls.Add(cmbDifficulty);
-            Controls.Add(txtSongName);
+            ClientSize = new Size(947, 570);
+            Controls.Add(btnLoad);
+            Controls.Add(dgvStats);
+            Controls.Add(cmbLevel);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dgvHistory).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvStats).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox txtSongName;
-        private ComboBox cmbDifficulty;
-        private Button btnSearch;
-        private DataGridView dgvHistory;
-        private Button btnShowGraph;
+        private ComboBox cmbLevel;
+        private DataGridView dgvStats;
+        private Button btnLoad;
     }
 }
